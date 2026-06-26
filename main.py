7dae74101a0e850078b7a99a4a5977c6e4c65760
Nilton <jjmilton4@hotmail.com>
2026-06-26 14:00:30 -0500

    Se agrego tipos de datos en Pyhton

---
+++ main.py
@@ -0,0 +1,26 @@
+ ##### Tipos de datos ######
+ #Enteros
+entero=10
+ #Flotantes 
+Flotante=3.14
+ #Cadenas de texto 
+Cadena= 'Hola, mundo'
+ #Boleanos
+Boleano= True
+Boleano2= False
+ #Listas
+Lista= [1,2,3,4,5] #Mutable
+ #Tuplas
+Tupla= [1,2,3,4,5] #inmutable
+#Diccionario
+Diccionario= {"nombre":"Juan","Edad": 30, "Ciuda":"Madrid"}#lleva valor 
+#Conjuntos 
+conjunto = {1,2,3,4,5}
+
+print(entero)
+print(Flotante)
+print(Boleano)
+print(Lista)
+print(Tupla)
+print(Diccionario)
+print(conjunto)
